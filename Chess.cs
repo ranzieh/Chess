315dd283bf795bf4dc6f0265137e0633fef0c6a3
ranzieh <ranziehen@gmail.com>
2021-04-30 19:46:13 +0200

    started on pawn movement

--- Chess.cs
+++ Chess.cs
@@ -6,6 +6,8 @@ namespace Chess
     {
         public static string startPosition="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
         public Hashtable board;
+        private bool whitesTurn;
+        private string reasonForInvalidMove;
 
         public Game() : this(startPosition)
         {
@@ -25,7 +27,7 @@ namespace Chess
             string boardInfo=info[0];
             string[] rows=boardInfo.Split("/");  
 
-            int currentRake = 8;
+            int currentRank = 8;
             foreach (string row in rows)
             {
                 int currentFile = 1;
@@ -39,21 +41,115 @@ namespace Chess
                     }
                     else
                     {
-                        int pos = currentFile * 10 + currentRake; 
+                        int pos = currentFile * 10 + currentRank; 
                         board.Add(pos,c);
                         currentFile++;
                     }
                 }
-                currentRake--;
+                currentRank--;
+            }
+
+            if (info.Length>1)
+            {
+                whitesTurn = info[1]=="w";
+            }
+            else
+            {
+                whitesTurn=true;
             }
         }
-        
+
+        internal bool IsWhitesTurn()
+        {
+            return whitesTurn;
+        }
+
+        internal bool PlayMoveAN(string move, bool checkOnly=false)
+        {
+            if (Char.IsUpper(move[0]))
+            {
+                //non-pawn move
+                throw new NotImplementedException("Non pawn moves are not implemented yet. Try a pawn move!");
+            }
+            else
+            {
+                //set direction in shich the pawn is moving. 1 if whites turn -1 if blacks turn
+                int direction = whitesTurn? 1 : -1;
+
+                //check if pawn is capturing
+                if (move.Length>2 && move[2]=='x')
+                {
+                    //pawn is capturing
+                }
+                else
+                {
+                    int targetpos = Utility.GetNumericPosFromAN(move.Substring(0,2));
+                    if (board[targetpos]==null)
+                    {
+                        int currentpos = targetpos-direction;
+                        if (Utility.IsPawn((char)board[currentpos], whitesTurn))
+                        {
+                            Move(currentpos,targetpos);
+                        }
+                        else
+                        {
+                            if ((targetpos%10==4 && whitesTurn) || (targetpos%10==5 && !whitesTurn))
+                            {
+                                if (board[currentpos]==null)
+                                {                                    
+                                    reasonForInvalidMove="A Piece is on the Square between your Pawn and the target Square.";
+                                    return false;
+                                }
+                                currentpos-=direction;
+                                if (Utility.IsPawn((char)board[currentpos], whitesTurn))
+                                {
+                                    Move(currentpos,targetpos);                                    
+                                }
+                            }
+                            reasonForInvalidMove="There is no Pawn that can move to the specified Square.";
+                            return false;
+                        }                        
+                    }
+                    else
+                    {
+                        reasonForInvalidMove="A Piece is on the Square you are trying to move to.";
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void Move(int currentpos, int targetpos)
+        {
+            board[targetpos]=board[currentpos];
+            board[currentpos]=null;
+        }
     }
     static class Utility
     {
         public static bool isWhitePiece(char piece)
         {
             return !Char.IsLower(piece);
-        }        
+        }
+
+        internal static int GetNumericPosFromAN(string v)
+        {
+            int file = Char.ToUpper(v[0])-64;
+            int rank = Convert.ToInt32(Char.GetNumericValue(v[1]));
+            int rv = file * 10 + rank;
+            return rv;
+        }
+
+        internal static bool IsPawn(char piece, bool isWhite)
+        {
+            if (piece == null)
+                return false;
+
+            if (isWhite)
+                return piece=='P';
+            else
+                return piece=='p';
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,7 +11,27 @@ namespace Chess
                 game = new Game();
             else
                 game = new Game(args[0]);
-            VisualizeBoard(game);
+            
+            Console.WriteLine("Chess by ranzieh: https://github.com/ranzieh/Chess");
+            Console.WriteLine("Enter moves in Algebraic Notation (https://en.wikipedia.org/wiki/Algebraic_notation_(chess))");
+            Console.WriteLine("Have Fun!");
+            bool playing = true;
+            while (playing)
+            {
+                VisualizeBoard(game);
+                string turn = game.IsWhitesTurn()? "White" : "Black";
+                Console.WriteLine($"It is {turn}'s turn. Please enter your move:");
+                string move = Console.ReadLine();
+                if (move=="exit")
+                {
+                    playing=false;
+                    continue;
+                }
+                else
+                {
+                    game.PlayMoveAN(move);
+                }
+            }
         }
 
 
